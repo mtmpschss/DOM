b5d50b0c7a4ecab235a443156a1fa5a117bac706
Kamil 1st <kamilferst@wp.pl>
2018-01-13 10:24:30 +0100

    cwiczenie pobieranie elementow

---
+++ js/2-odnajdywanie-elementow.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var parFirst = document.getElementById("parFirst");
+
+
+var linki = document.getElementsByClassName("superlink");
+//console.log(linki);
+
+var linkiPoTagu = document.getElementsByTagName("a");
+//console.log(linkiPoTagu);
+
+var divPoId = document.querySelector("#parSecond");
+//console.log(divPoId);
+
+var superlinki = document.querySelectorAll(".superlink");
+console.log(superlinki);
