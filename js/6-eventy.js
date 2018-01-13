1540f550ff0a3386eb63b73c72c7892b153c4345
Kamil 1st <kamilferst@wp.pl>
2018-01-13 14:27:56 +0100

    eventy

---
+++ js/6-eventy.js
@@ -0,0 +1,40 @@
+
+var secondLink = document.getElementsByTagName("a")[1];
+
+function alarm(e){
+    'use strict';
+    e.preventDefault();
+    console.log("Kliknieto link")
+    console.log(e.type)
+}
+
+//secondLink.onclick = alarm;
+
+//secondLink.addEventListener('Click', alarm);
+//secondLink.removeEventListener('click', alarm);
+
+var mainHeader = document.getElementById("main-header");
+
+function zmianaRozmiaru(e) {
+    if(e.type == "mouseover") {
+        mainHeader.style.fontSize = "70px";        
+    } else {
+        mainHeader.style.fontSize = "inherit";
+    }
+}
+
+mainHeader.addEventListener("mouseover", zmianaRozmiaru);
+mainHeader.addEventListener("mouseout", zmianaRozmiaru);
+
+
+document.getElementById("exampleDiv").addEventListener ('click', function(e) {
+    e.preventDefault();
+    console.log("Kliknięto w div!");
+});
+
+document.getElementById("exampleLink").addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("Kliknięto w link!");
+});
+
